b7f7ad7e410766e2261bb490968c35ea0d034794
liu <15883111679@163.com>
2021-06-09 20:51:15 +0800

    update Code04_BestArrange

--- Basics/class_07/Code04_BestArrange.java
+++ Basics/class_07/Code04_BestArrange.java
@@ -3,6 +3,8 @@ package Basics.class_07;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static sun.misc.Version.print;
+
 public class Code04_BestArrange {
 
 	public static class Program {
@@ -28,7 +30,7 @@ public class Code04_BestArrange {
 		Arrays.sort(programs, new ProgramComparator());
 		int result = 0;
 		for (int i = 0; i < programs.length; i++) {
-			if (start <= programs[i].start) {
+			if(start <= programs[i].start){
 				result++;
 				start = programs[i].end;
 			}
@@ -37,7 +39,15 @@ public class Code04_BestArrange {
 	}
 
 	public static void main(String[] args) {
-
+		Program[] programs = new Program[6];
+		programs[0] = new Program(0, 3);
+		programs[1] = new Program(2, 4);
+		programs[2] = new Program(4, 9);
+		programs[3] = new Program(5, 7);
+		programs[4] = new Program(6, 10);
+		programs[5] = new Program(10, 12);
+		int count = bestArrange(programs, 0);
+		System.out.println("最多能安排" + count + "个项目");
 	}
 
 }
